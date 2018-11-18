566d38a4d08d30968e744fb0d5ab0b7fbbb890c8
prasanna <pp723@njit.edu>
2018-11-18 02:34:07 -0500

    multiple modifications. Bug fixes. Database updated

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-18 06:37:16.
+    * Generated on 2018-11-18 07:11:30.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper

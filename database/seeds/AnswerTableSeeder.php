312d193da0c7ff4b6f18ac744ee0ec10c06a687e
prasanna <pp723@njit.edu>
2018-11-20 21:10:26 -0500

    AnswerTableSeeder.php, question.blade.php, web.php edited answer.blade.php created.
    View answer method created.

--- database/seeds/AnswerTableSeeder.php
+++ database/seeds/AnswerTableSeeder.php
@@ -12,7 +12,7 @@ class AnswerTableSeeder extends Seeder
     public function run()
     {
         $users = App\User::all();
-
+        for ($i = 1; $i <= 6; $i++) {
             $users->each(function ($user) {
                 $question = App\Question::inRandomOrder()->first();
                 $answer = factory(\App\Answer::class)->make();
@@ -20,5 +20,7 @@ class AnswerTableSeeder extends Seeder
                 $answer->question()->associate($question);
                 $answer->save();
             });
+
+        }
     }
 }
